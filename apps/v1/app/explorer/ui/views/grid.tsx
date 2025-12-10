9699da039d63cfcdde075cb7afc6729c0ce918c9
nishino <nishino2nd@outlook.jp>
2025-12-10 02:30:29 +0000

    GridView のテキストをリンクに含めないように修正

--- apps/v1/app/explorer/ui/views/grid.tsx
+++ apps/v1/app/explorer/ui/views/grid.tsx
@@ -11,16 +11,17 @@ export function GridView({
   return (
     <div className="grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-6">
       {data.map((node) => (
-        <Link
-          key={node.path}
-          href={node.isDirectory ? "/explorer/" + node.path : "#"}
-          className="cursor-pointer"
-        >
-          <div className="aspect-square overflow-hidden rounded-lg border bg-muted">
-            <MediaThumb node={node} />
-          </div>
+        <div key={node.path}>
+          <Link
+            href={node.isDirectory ? "/explorer/" + node.path : "#"}
+            className="cursor-pointer"
+          >
+            <div className="aspect-square overflow-hidden rounded-lg border bg-muted">
+              <MediaThumb node={node} />
+            </div>
+          </Link>
           <div className="mt-1 truncate text-center text-xs">{node.name}</div>
-        </Link>
+        </div>
       ))}
     </div>
   );
