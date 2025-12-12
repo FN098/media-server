3ddac3553b1fab95b9e02ce27ea170044e7652ae
nishino <nishino2nd@outlook.jp>
2025-12-12 12:48:08 +0000

    typescript-eslint を導入
    https://typescript-eslint.io/getting-started/typed-linting/

--- apps/v1/eslint.config.mjs
+++ apps/v1/eslint.config.mjs
@@ -1,10 +1,19 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
