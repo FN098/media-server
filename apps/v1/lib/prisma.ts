3a10a0817f34661ee2f954b313c085b00fb90717
nishino <nishino2nd@outlook.jp>
2025-12-27 03:35:56 +0000

    コネクションプールを 5 に戻す
    接続数が多くてもリソースリークになれば意味がないため

--- apps/v1/lib/prisma.ts
+++ apps/v1/lib/prisma.ts
@@ -11,7 +11,7 @@ const adapter = new PrismaMariaDb({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  connectionLimit: 30,
+  connectionLimit: 5,
 });
 
 const globalForPrisma = globalThis as unknown as {
