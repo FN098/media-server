4c731c0ead1f71508619612a49baffd1cf4314d7
nishino <nishino2nd@outlook.jp>
2026-01-02 19:45:25 +0000

    タグ検索APIリファクタ

--- apps/v1/app/api/tags/route.ts
+++ apps/v1/app/api/tags/route.ts
@@ -12,6 +12,8 @@ const RequestSchema = z.object({
   limit: z.coerce.number().optional().default(MAX_RETURN_TAGS_COUNT),
 });
 
+type RequestParams = z.infer<typeof RequestSchema>;
+
 export async function GET(request: NextRequest) {
   try {
     const { searchParams } = request.nextUrl;
@@ -24,27 +26,7 @@ export async function GET(request: NextRequest) {
       return new NextResponse("Invalid paths format", { status: 400 });
     }
 
-    // パスが多すぎる場合は、先頭からカットして処理（DB負荷対策）
-    const paths = parsed.data.paths.slice(0, MAX_PATHS_TO_PROCESS);
-
-    const limit = MAX_RETURN_TAGS_COUNT;
-
-    // 1. 関連タグの取得
-    const relatedTags = await getRelatedTags(paths, { limit });
-
-    const excludeIds = relatedTags.map((t) => t.id);
-    const remain = limit - relatedTags.length;
-
-    // 2. その他（人気）タグの取得
-    const popularTags = await searchTags({
-      excludeIds,
-      limit: remain,
-      query: parsed.data.query,
-      strategy: parsed.data.strategy,
-    });
-
-    // 3. マージ
-    const result = [...relatedTags, ...popularTags];
+    const result = await process(parsed.data);
 
     return NextResponse.json(result);
   } catch (error) {
@@ -63,31 +45,35 @@ export async function POST(request: NextRequest) {
       return new NextResponse("Invalid request body", { status: 400 });
     }
 
-    // パスが多すぎる場合は、先頭からカットして処理（DB負荷対策）
-    const paths = parsed.data.paths.slice(0, MAX_PATHS_TO_PROCESS);
-
-    const limit = parsed.data.limit;
-
-    // 1. 関連タグの取得
-    const relatedTags = await getRelatedTags(paths, { limit });
-
-    const excludeIds = relatedTags.map((t) => t.id);
-    const remain = limit - relatedTags.length;
-
-    // 2. その他（人気）タグの取得
-    const popularTags = await searchTags({
-      excludeIds,
-      limit: remain,
-      query: parsed.data.query,
-      strategy: parsed.data.strategy,
-    });
-
-    // 3. マージ
-    const result = [...relatedTags, ...popularTags];
+    const result = await process(parsed.data);
 
     return NextResponse.json(result);
   } catch (error) {
     console.error("Tag Fetch Error:", error);
     return new NextResponse("Internal Server Error", { status: 500 });
   }
+}
+
+async function process(params: RequestParams) {
+  // パスが多すぎる場合は、先頭からカットして処理（DB負荷対策）
+  const paths = params.paths.slice(0, MAX_PATHS_TO_PROCESS);
+  const limit = params.limit;
+
+  // 1. 関連タグの取得
+  const relatedTags = await getRelatedTags(paths, { limit });
+  const excludeIds = relatedTags.map((t) => t.id);
+  const remain = limit - relatedTags.length;
+
+  // 2. その他（人気）タグの取得
+  const popularTags = await searchTags({
+    excludeIds,
+    limit: remain,
+    query: params.query,
+    strategy: params.strategy,
+  });
+
+  // 3. マージ
+  const merged = [...relatedTags, ...popularTags];
+
+  return merged;
 }
