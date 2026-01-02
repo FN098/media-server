112bad7c33fea24cd636e03694868e2a6cecb0d2
nishino <nishino2nd@outlook.jp>
2026-01-02 16:02:34 +0000

    @prisma/adapter-mariadb は不安定なので使わない

--- apps/v1/lib/prisma.ts
+++ apps/v1/lib/prisma.ts
@@ -3,22 +3,12 @@
  */
 
 import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import "dotenv/config";
-
-const adapter = new PrismaMariaDb({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  connectionLimit: 5,
-});
 
 export const prisma =
   globalThis.prisma ??
   new PrismaClient({
-    adapter,
-    log: ["error"], // optional
+    log: ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
