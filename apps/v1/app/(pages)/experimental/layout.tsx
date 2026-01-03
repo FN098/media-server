23f126bd971df991b8325ee0291cb6eafe0c846f
nishino <nishino2nd@outlook.jp>
2026-01-03 18:34:04 +0000

    experimentalページのエラー修正

--- apps/v1/app/(pages)/experimental/layout.tsx
+++ apps/v1/app/(pages)/experimental/layout.tsx
@@ -4,7 +4,15 @@ import { PATHS } from "@/lib/path/paths";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header title="Experimental" basePath={PATHS.client.experimental.root} />
+      <Header
+        title="Experimental"
+        basePath={PATHS.client.experimental.root}
+        features={{
+          navigation: false,
+          search: false,
+          viewMode: false,
+        }}
+      />
       <main className="flex-1 flex items-center justify-center">
         {children}
       </main>
