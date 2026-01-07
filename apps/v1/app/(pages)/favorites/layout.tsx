762f4b38d3bb22bdc68902f3024d7e1e58554452
nishino <nishino2nd@outlook.jp>
2026-01-07 13:53:37 +0000

    ViewerProvider 追加漏れ

--- apps/v1/app/(pages)/favorites/layout.tsx
+++ apps/v1/app/(pages)/favorites/layout.tsx
@@ -3,17 +3,23 @@ import { PATHS } from "@/lib/path/paths";
 import { SearchProvider } from "@/providers/search-provider";
 import { TagEditorProvider } from "@/providers/tag-editor-provider";
 import { ViewModeProvider } from "@/providers/view-mode-provider";
+import { ViewerProvider } from "@/providers/viewer-provider";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ViewModeProvider>
       <SearchProvider>
-        <TagEditorProvider>
-          <div className="flex flex-col min-h-screen">
-            <Header title="Favorites" basePath={PATHS.client.favorites.root} />
-            <main className="flex-1 flex">{children}</main>
-          </div>
-        </TagEditorProvider>
+        <ViewerProvider>
+          <TagEditorProvider>
+            <div className="flex flex-col min-h-screen">
+              <Header
+                title="Favorites"
+                basePath={PATHS.client.favorites.root}
+              />
+              <main className="flex-1 flex">{children}</main>
+            </div>
+          </TagEditorProvider>
+        </ViewerProvider>
       </SearchProvider>
     </ViewModeProvider>
   );
