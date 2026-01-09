346dab57b5f0101c4613d5fe7ea71b93a400139c
nishino <nishino2nd@outlook.jp>
2026-01-09 09:45:32 +0000

    prisma バージョンアップ v6.19.1 -> v7.2.0

--- apps/v1/lib/prisma.ts
+++ apps/v1/lib/prisma.ts
@@ -1,16 +1,29 @@
 /**
+ * @see https://www.prisma.io/docs/orm/prisma-client/setup-and-configuration/no-rust-engine
  * @see https://www.prisma.io/docs/getting-started/prisma-orm/quickstart/mysql
  */
 
 import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import "dotenv/config";
 
-export const prisma =
+const adapter = new PrismaMariaDb({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  connectionLimit: 5,
+});
+
+const prisma =
   globalThis.prisma ??
   new PrismaClient({
+    adapter,
     log: ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = prisma;
 }
+
+export { prisma };
