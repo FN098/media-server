42ade111005387e303a279f227b8f5f21e68ea9a
nishino <nishino2nd@outlook.jp>
2026-01-12 13:50:37 +0000

    prisma の maria db adapter の設定を修正
    公開キーの取得を許可する設定を有効化しないと、mysql のRSA公開キーの取得ができず認証プロセスに失敗し、接続ができなくなる問題があった。なぜか prisma cli で接続したあとはRSA鍵の交換がスキップされていたので、問題が顕在化しなかった。

--- apps/v1/lib/prisma.ts
+++ apps/v1/lib/prisma.ts
@@ -1,6 +1,7 @@
 /**
  * @see https://www.prisma.io/docs/orm/prisma-client/setup-and-configuration/no-rust-engine
  * @see https://www.prisma.io/docs/getting-started/prisma-orm/quickstart/mysql
+ * @see https://github.com/prisma/prisma/issues/28612
  */
 
 import { PrismaClient } from "@/generated/prisma/client";
@@ -13,6 +14,23 @@ const adapter = new PrismaMariaDb({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   connectionLimit: 5,
+  allowPublicKeyRetrieval: true,
+
+  // デバッグ用
+  // logger: {
+  //   network: (info) => {
+  //     console.log("PrismaAdapterNetwork", info);
+  //   },
+  //   query: (info) => {
+  //     console.log("PrismaAdapterQuery", info);
+  //   },
+  //   error: (error) => {
+  //     console.error("PrismaAdapterError", error);
+  //   },
+  //   warning: (info) => {
+  //     console.warn("PrismaAdapterWarning", info);
+  //   },
+  // },
 });
 
 const prisma =
