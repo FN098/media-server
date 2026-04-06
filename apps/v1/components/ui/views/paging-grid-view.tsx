143973ea566b1c266e569b0d6823b0588cdae8d8
nishino <nishino2nd@outlook.jp>
2026-04-06 17:11:57 +0000

    アクションメニューのアイコンを右揃え

--- apps/v1/components/ui/views/paging-grid-view.tsx
+++ apps/v1/components/ui/views/paging-grid-view.tsx
@@ -316,7 +316,7 @@ function Cell({
           </div>
 
           {/* Actions */}
-          <div className="absolute top-2 right-2 flex flex-col gap-2">
+          <div className="absolute top-2 right-2 flex flex-col items-end gap-2">
             {!selectCtx.isSelectionMode && isMediaNode && onRatingChange && (
               <ToggleFavoriteButton
                 variant="grid"
