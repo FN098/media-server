719de69293095248801986b9d425c375037485e6
nishino <nishino2nd@outlook.jp>
2026-04-13 07:13:49 +0000

    移動ダイアログのボタンテキスト修正

--- apps/v1/components/ui/dialogs/move-dialog.tsx
+++ apps/v1/components/ui/dialogs/move-dialog.tsx
@@ -169,7 +169,7 @@ export function MoveDialog({
             ) : (
               <>
                 <FolderInput className="mr-2 h-4 w-4" />
-                ここにコピー
+                ここに移動
               </>
             )}
           </Button>
