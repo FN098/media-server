96b37cc7ae02937adecab081227d797652ba6cef
nishino <nishino2nd@outlook.jp>
2026-05-28 09:44:20 +0000

    tag-filter-dialog 修正

--- apps/v1/components/ui/dialogs/tag-filter-dialog.tsx
+++ apps/v1/components/ui/dialogs/tag-filter-dialog.tsx
@@ -258,7 +258,7 @@ export function TagFilterDialog({
       mode: currentMode,
       tags: tempSelectedTags,
     });
-    onOpenChange(false);
+    handleOpenChange(false);
   };
 
   const mounted = useMounted();
