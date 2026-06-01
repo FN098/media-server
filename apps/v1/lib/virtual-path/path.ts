a6d81570473a4d360621652f3e708eea633b38dc
nishino <nishino2nd@outlook.jp>
2026-06-01 00:11:32 +0000

    sanitize 関数追加

--- apps/v1/lib/virtual-path/path.ts
+++ apps/v1/lib/virtual-path/path.ts
@@ -2,10 +2,17 @@ import pathPosix from "path/posix";
 
 // path/posix のラッパーだが、一貫性と拡張性のために使用
 
-export function join(...paths: string[]) {
+export function join(...paths: string[]): string {
   return pathPosix.join(...paths);
 }
 
-export function dirname(path: string) {
+export function dirname(path: string): string {
   return pathPosix.dirname(path);
+}
+
+export function sanitize(path: string): string {
+  return path
+    .replace(/\\/g, "/") // \ → /
+    .replace(/\/+/g, "/") // // → /
+    .replace(/^\/+|\/+$/g, ""); // 先頭・末尾の /
 }
