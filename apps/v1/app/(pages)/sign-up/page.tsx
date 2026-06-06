c2b71961b857b4d3180bbe43c015db7acf4e9947
nishino <nishino2nd@outlook.jp>
2026-06-06 12:02:05 +0000

    SignUp ページを動的生成するオプション追加

--- apps/v1/app/(pages)/sign-up/page.tsx
+++ apps/v1/app/(pages)/sign-up/page.tsx
@@ -1,6 +1,8 @@
 import { SignUp } from "@/components/ui/pages/sign-up";
 import { db } from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export default async function SignUpPage() {
   const admin = await db.user.findFirst({ where: { role: "admin" } });
 
