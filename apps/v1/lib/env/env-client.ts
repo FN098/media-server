a49245debdac171865d377e2861478f4e3aefe57
nishino <nishino2nd@outlook.jp>
2026-06-06 09:20:49 +0000

    env-client 更新

--- apps/v1/lib/env/env-client.ts
+++ apps/v1/lib/env/env-client.ts
@@ -8,6 +8,18 @@ export function getWebUiUrl(): string | null {
   return publicEnv.NEXT_PUBLIC_WEB_UI_URL ?? null;
 }
 
+export function getWebUiUrlOrThrow(): string {
+  const url = getWebUiUrl();
+  if (!url) throw new Error("Missing env:NEXT_PUBLIC_WEB_UI_URL");
+  return url;
+}
+
 export function getWebApiUrl(): string | null {
   return publicEnv.NEXT_PUBLIC_WEB_API_URL ?? null;
+}
+
+export function getWebApiUrlOrThrow(): string {
+  const url = getWebApiUrl();
+  if (!url) throw new Error("Missing env:NEXT_PUBLIC_WEB_API_URL");
+  return url;
 }
