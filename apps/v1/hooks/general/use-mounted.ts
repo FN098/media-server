02c3644cc0791c7682b6dc9aaa8c3b47791cbd0c
nishino <nishino2nd@outlook.jp>
2026-06-08 13:53:12 +0000

    use-mounted 修正

--- apps/v1/hooks/general/use-mounted.ts
+++ apps/v1/hooks/general/use-mounted.ts
@@ -1,14 +1,12 @@
-import { useEffect, useState } from "react";
+import { startTransition, useEffect, useState } from "react";
 
 export function useMounted() {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    // 実行を次のイベントループに回す
-    const frame = requestAnimationFrame(() => {
+    startTransition(() => {
       setMounted(true);
     });
-    return () => cancelAnimationFrame(frame);
   }, []);
 
   return mounted;
