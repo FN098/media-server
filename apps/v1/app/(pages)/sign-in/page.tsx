ac7765f0b6713a830bf6fb53e99087b393909190
nishino <nishino2nd@outlook.jp>
2026-06-09 12:32:50 +0000

    sign-in レイアウト修正

--- apps/v1/app/(pages)/sign-in/page.tsx
+++ apps/v1/app/(pages)/sign-in/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@/components/ui/pages/sign-in";
 
 export default function SignInPage() {
-  return <SignIn />;
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <SignIn />
+    </div>
+  );
 }
