592510d05aa32d78fe12600a1d4d44ece800ba45
nishino <nishino2nd@outlook.jp>
2026-06-09 14:37:48 +0000

    sign-in/up のフェード効果削除

--- apps/v1/app/(pages)/sign-in/page.tsx
+++ apps/v1/app/(pages)/sign-in/page.tsx
@@ -1,20 +1,9 @@
-import { MotionDiv } from "@/components/ui/framer-motion/motion-div";
 import { SignIn } from "@/components/ui/pages/sign-in";
 
 export default function SignInPage() {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <MotionDiv
-        initial={{ opacity: 1, y: 12 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0,
-          duration: 0.4,
-          ease: "easeOut",
-        }}
-      >
-        <SignIn />
-      </MotionDiv>
+      <SignIn />
     </div>
   );
 }
--- apps/v1/app/(pages)/sign-up/page.tsx
+++ apps/v1/app/(pages)/sign-up/page.tsx
@@ -1,4 +1,3 @@
-import { MotionDiv } from "@/components/ui/framer-motion/motion-div";
 import { SignUp } from "@/components/ui/pages/sign-up";
 import { db } from "@/lib/prisma";
 
@@ -12,17 +11,7 @@ export default async function SignUpPage() {
 
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <MotionDiv
-        initial={{ opacity: 1, y: 12 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0,
-          duration: 0.4,
-          ease: "easeOut",
-        }}
-      >
-        <SignUp hasAdmin={!!admin} />
-      </MotionDiv>
+      <SignUp hasAdmin={!!admin} />
     </div>
   );
 }
