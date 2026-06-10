2898fcbd07b83903979f76af1fcc05b67fed2eb5
nishino <nishino2nd@outlook.jp>
2026-06-10 03:16:05 +0000

    ビルドエラー修正。useSearchParams を使うコンポーネントを Suspense で囲む

--- apps/v1/app/(pages)/sign-in/page.tsx
+++ apps/v1/app/(pages)/sign-in/page.tsx
@@ -1,9 +1,12 @@
 import { SignIn } from "@/components/ui/pages/sign-in";
+import { Suspense } from "react";
 
 export default function SignInPage() {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <SignIn />
+      <Suspense>
+        <SignIn />
+      </Suspense>
     </div>
   );
 }
