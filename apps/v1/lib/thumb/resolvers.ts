2d324cef06b2391e05ceda35758b60addca24162
nishino <nishino2nd@outlook.jp>
2026-06-10 22:07:34 +0000

    resolveMediaThumbUrl リファクタリング

--- apps/v1/lib/thumb/resolvers.ts
+++ apps/v1/lib/thumb/resolvers.ts
@@ -6,25 +6,24 @@ type Media = {
   isDeleted?: boolean;
 };
 
-type UrlOption = {
+type UrlOptions = {
   absolute?: boolean;
   version?: number;
 };
 
-export function resolveMediaThumbUrl(media: Media, option?: UrlOption) {
+export function resolveMediaThumbUrl(media: Media, options?: UrlOptions) {
   // ファイルを削除してもサムネイルはそのまま残るので同じパスを参照
   const basePath = media.path;
   const encoded = encodePath(basePath);
 
-  const url = option?.absolute
+  const url = options?.absolute
     ? getAbsoluteApiThumbUrl(encoded)
     : getApiThumbPath(encoded);
 
-  return appendVersion(url, option?.version);
+  return options?.version ? appendVersion(url, options.version) : url;
 }
 
-function appendVersion(url: string, version?: number) {
-  if (!version) return url;
+function appendVersion(url: string, version: number) {
   const separator = url.includes("?") ? "&" : "?";
   return `${url}${separator}v=${version}`;
 }
