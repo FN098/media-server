355f084cc33dba25d931b01d99fef238579e30c3
nishino <nishino2nd@outlook.jp>
2026-06-11 08:23:00 +0000

    使われていない props 削除

--- apps/v1/hooks/dropdown-menus/use-node-context-menu.ts
+++ apps/v1/hooks/dropdown-menus/use-node-context-menu.ts
@@ -12,8 +12,6 @@ const transformer = createRecursiveTransformer<
 interface UseNodeDropdownMenuProps {
   node: MediaNode;
   menuItems: MenuItemDef<NodeContext>[];
-  onOpenChange?: (open: boolean) => void;
-  disabled?: boolean;
 }
 
 export function useNodeDropdownMenu({
