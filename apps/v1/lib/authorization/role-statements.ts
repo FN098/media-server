70d57f453c06cdfcdcaa0a76bc75c5a1a4c24359
nishino <nishino2nd@outlook.jp>
2026-06-11 08:13:07 +0000

    コメント追加

--- apps/v1/lib/authorization/role-statements.ts
+++ apps/v1/lib/authorization/role-statements.ts
@@ -2,6 +2,12 @@ import { Statement, statements } from "@/lib/authorization/statements";
 import { Role } from "@/lib/user/roles";
 
 export const roleStatements = {
-  admin: statements,
-  user: [],
+  admin: [
+    // 管理者ユーザーは定義済みの全ステートメントを許可
+    ...statements,
+  ],
+  user: [
+    // 一般ユーザーに許可する権限を個別に許可
+    // 現状はすべての操作を禁止
+  ],
 } as const satisfies Record<Role, readonly Statement[]>;
